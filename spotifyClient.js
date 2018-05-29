d54ad3f145dbb70f73b5dac83f80024bb2ac5af8
MaarLop <s.mariel.lopez1995@gmail.com>
2018-05-29 15:22:58 -0300

    spot

---
+++ spotifyClient.js
@@ -0,0 +1,22 @@
+const unqfy= require ('./unqfy.js');
+
+const url_base= 'https://api.spotify.com/v1/artists/';
+
+getAlbumForArtist(artistid){
+    const rp = require('request-promise');
+    const options = {
+           url: + artistid+ '/albums',
+           headers: { Authorization: 'Bearer ' + this.accestoke 'ACCESS_TOKEN' },
+           json: true
+          };
+           rp.get(options).then((response) => //hacer algo con response);
+    }
+  }
+
+  popularAlbumForArtist(artistname){
+
+  }
+
+  getLyric(){
+
+  }
