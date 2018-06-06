a9c92f4df2d07e2388a2d200e7316e46303df4f9
MaarLop <s.mariel.lopez1995@gmail.com>
2018-06-06 00:58:13 -0300

    se soluciono parcialmente lo pedido para el cliente de spotify

--- spotifyClient.js
+++ spotifyClient.js
@@ -1,13 +1,29 @@
-const unqfy= require ('./unqfy.js');
+const unqmod= require ('./unqfy.js');
 const fs = require('fs');
 const access_token= JSON.parse(fs.readFileSync('./spotifyCreds.json', 'utf8')).access_token;
 const url_base= 'https://api.spotify.com/v1';
 
 const rp = require('request-promise');
 
+function getUNQfy(filename) {
+    let unqfy = new unqmod.UNQfy();
+    if (fs.existsSync(filename)) {
+      console.log();
+      unqfy = unqmod.UNQfy.load(filename);
+    }
+    return unqfy;
+  }
+  
+  // Guarda el estado de UNQfy en filename
+  function saveUNQfy(unqfy, filename) {
+    console.log();
+    unqfy.save(filename);
+  }
+  const unqfy= getUNQfy ('unqfy.json');
+
 function getAlbumForArtist(artistName){
-    const promise_artistid= getIdOfArtist(artistName);
-    promise_artistid.then( (id)=>
+    let promise_artistid = getIdOfArtist(artistName);
+    promise_artistid.then((id) =>
         {
             options = 
             {
@@ -16,19 +32,45 @@ function getAlbumForArtist(artistName){
                 json: true,
             }
             rp.get(options).then( (response) =>
-            {
-                for(let i=0; i<= json.size(); i++)
+            {   
+                let albums = response['items'];
+                for(let i=0; i< albums.length; i++)
                 {
-                    let albs=JSON.parse(response[i]);
-                    for (let i=0; i<= albs.size(); i++ )
-                    {
-                        let alb= albs[i];
-                        unqfy.addAlbum(artistName, {name:alb['name'], year:alb['release_date']})
-                    }
+                    let currentAlbum = albums[i];
+                    let title = currentAlbum['name'];
+                    let yearOf = currentAlbum['release_date'];
+                    unqfy.addAlbum(artistName, { name: title, year:yearOf })
+
                 }
+                saveUNQfy(unqfy,'unqfy.json')
             })
 
         })   
+}
+
+
+function popularAlbumForArtist(artistname){
+    let promise_artistid = getIdOfArtist(artistname);
+    promise_artistid.then ((id)=>
+    {
+        options = 
+        {
+            url: url_base+'/artists/' + id+ '/top-tracks',
+            headers: { Authorization: 'Bearer ' + access_token},
+            json: true,
+        };
+        rp.get(options).then( (response) =>
+        {
+            console.log(typeof response)
+            let track = response['tracks']
+            console.log(track)
+        //     let albumId = track['album'].id;
+        //     let alb= getAlbumWithId(albumId);
+        //    console.log (new Album ({name:alb['name'], year:alb['release_date']}));
+        });
+
+    })
+
 }
 
 function getIdOfArtist(name){
@@ -44,35 +86,17 @@ function getIdOfArtist(name){
         },
         json: true,
     };
+
     return rp.get(options).then( (response) =>
-    {
-        console.log(response['items'].id);
-    
-        
-     });
+    {        
+        let result = response;
+        return result['artists'].items[0].id;
+      }) 
      
 }
 
-function popularAlbumForArtist(artistname){
-    let artistid= getIdOfArtist(artistname);
-    const options = 
-    {
-       url: url_base+'/artist/' + artistid+ '/top-tracks',
-       headers: { Authorization: 'Bearer ' + access_token},
-       json: true,
-    };
-    rp.get(options).then( (response) =>
-    {
-        let track=(JSON.parse(response))[0];   
-        let albumId = trakc[album['id']];
-        let alb= getAlbumWithId(albumid);
-        unqfy.getAlbumByName( alb['name']);
-    });
-}
-
 function getAlbumWithId(id){
-    let artistid= getIdOfArtist(artistName);
-    const options = 
+    options = 
     {
         url: url_base+'/albums/' + id,
         headers: { Authorization: 'Bearer ' + access_token},
@@ -80,7 +104,8 @@ function getAlbumWithId(id){
     };
        rp.get(options).then( (response) =>
     {
-        return JSON.parse(json);
+        let album= response;
+        return album;
     });
 }
 module.exports= {
