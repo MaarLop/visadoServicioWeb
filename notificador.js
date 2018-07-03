4fd3196fe25c602968173d231bda869d70e4e998
gisele <escobargisele1@gmail.com>
2018-07-03 16:35:30 -0300

    agregado de notificador para service

---
+++ notificador.js
@@ -0,0 +1,45 @@
+const sendMmail = require('./sendMail');
+class Notificador{
+    constructor(){
+        this.subcripciones={};
+
+    }
+    subscribe(artId,email){
+        if (artId.includes(this.subcripciones)){
+            if(!this.subcripciones[artId].includes(email)){
+                this.subcripciones[artId].push(email);
+            }
+            
+        }
+        else{
+            this.subcripciones[artId]= [email.toLowerCase()];
+        }
+    }
+
+    unsubscribe(artId, email){
+        if(artId.includes(this.subcripciones)){
+            const index= this.subcripciones[artId].findIndex(email);
+            if(index != -1){
+                this.subcripciones.get(artId).delete([index]);
+            }
+        }
+        
+    }
+    notify(artId,subject, menssage, from){
+        
+        let artId= this.subcripciones.includes(artId);
+        try{
+            if(!artId){
+                throw new Error('No existe el artista');
+                
+            }
+            else{
+                sendMmail.send(subject, menssage,from, this.subcripciones.get(artId));
+            }
+        }
+        catch (e) 
+        {
+          console.log(e.message);
+        }
+    }
+}
---
+++ sendMail.js
@@ -0,0 +1,55 @@
+const nodemailer = require('nodemailer');
+const xoauth2 = require('xoauth2');
+
+const fs = require('fs');
+const email= JSON.parse(fs.readFileSync('./mail.json', 'utf8')).user;
+const password= JSON.parse(fs.readFileSync('./mail.json', 'utf8')).pass;
+const client_id= JSON.parse(fs.readFileSync('./mail.json', 'utf8')).clientId;
+const client_secret= JSON.parse(fs.readFileSync('./mail.json', 'utf8')).clientSecret;
+
+const unqfymodule= require('./unqfy');
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com', // server para enviar mail desde gmail
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: email,
+        pass: password,
+        clientId: client_id,
+        clientSecret: client_secret,
+    },
+
+});
+
+function send (subject,message,lista){
+    const mailOptions = {
+        from: '"Subscription Service" '+'<'+email+'>', // sender address
+        to: lista,
+        subject: subject,
+        text: message, // plain text body
+        html: '<b>'+message+'</b>' // html body
+        }
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+            } 
+            else {
+                console.log(info);
+            }
+        });
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+            } 
+            else {
+                console.log(info);
+            }
+        });
+    }
+
+
+
+module.exports={
+    send
+}
