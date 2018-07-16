91c2c52bcda9355603444ad7ba151f6d57913663
MaarLop <s.mariel.lopez1995@gmail.com>
2018-07-16 09:47:50 -0300

    Limpieza

--- twitterClient.js
+++ twitterClient.js
@@ -4,31 +4,18 @@ const rp = require('request-promise');
 
 var client = new Twitter({
     consumer_key: 'KrlXVqNmu3geyE15neKFKizS4',
-  consumer_secret: 't2Ylj5vfk0AzmgJDNOHDjCEpisGfHL1LM39E93x9Og6ocDNYCv',
-  access_token_key: '1016511750773116928-RDVhyVdzNjDdnfhZLzT7BIkR6mj8E1',
-  access_token_secret: 'Y7OZ3CkFiCczJ5SfrdfeONDDpaa8069jaKSQmWA6NhXHZ',
+    consumer_secret: 't2Ylj5vfk0AzmgJDNOHDjCEpisGfHL1LM39E93x9Og6ocDNYCv',
+    access_token_key: '1016511750773116928-RDVhyVdzNjDdnfhZLzT7BIkR6mj8E1',
+    access_token_secret: 'Y7OZ3CkFiCczJ5SfrdfeONDDpaa8069jaKSQmWA6NhXHZ',
   });
    
-  var params = {screen_name: 'nodejs'};
   function get (name){
+  var params = {screen_name: name};
   return client.get('statuses/user_timeline', params).then(function(tweets) {
      return(tweets[0].text);
     })
   }
-
-// function get(name){
-//     let lista= []
-//     let params= {
-//         q: name,
-//         count: 1
-//     }
-
-//    return  client.getSearchTweets(params).then(function(data) {
-//     return data.statuses[0]
-//     })
-
-        
-//}
+     
 module.exports={
     get
 }
